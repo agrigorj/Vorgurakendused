f501f63158469f57afa5ef86176125746e2a15f5
Andrei Grigorjev <agrigorj@itcollege.ee>
2016-04-10 16:44:11 +0300

    8. ylesanne, p.2 valmis

--- 8.koduylesanne/oma_stiil.php
+++ 8.koduylesanne/oma_stiil.php
@@ -3,16 +3,12 @@
 <head>
   <meta charset="utf-8" />
   <title>8. kodutöö</title>
-  <style>
- .result {
-    color: <?php echo $txt_color; ?>;
-    background-color: <?php $bg_color;?>;
-    text-align: left;
-} 
-</style>
+ 
 </head>
 
 <body>
+
+ 
 <?php 
 $txt_color="#fff"; // vaikimisi valge
 $bg_color="#fff"; // vaikimisi valge
@@ -34,6 +30,30 @@ if (isset($_POST['b_color']) && $_POST['b_color']!="") {
     $border_col=htmlspecialchars($_POST['b_color']);
 } 
 ?>
+<style scoped>
+ .result {
+    color: <?php echo $txt_color; ?>;
+    background-color: <?php echo $bg_color; ?>;
+    border-style: <?php echo $border_style; ?>;
+    border-color: <?php echo $border_col; ?>;
+    border-width: <?php echo $border_weight; ?>px;
+    border-radius: <?php echo $border_radius; ?>px;
+    width: 500px;
+    min-height: 100px;
+    text-align: center;
+    margin-left: 20px;
+    margin-top: 20px;
+    padding-left: 20px;
+    padding-bottom: 20px;
+    padding-right: 20px;
+    padding-top: 20px;
+    font-size: 30px;
+    font-family: "Times New Roman", Georgia, Serif;
+    
+    
+    
+} 
+</style>
 <p class="result">
 <?php
 if (isset($_POST['user']) && $_POST['user']!="") {
@@ -41,27 +61,30 @@ if (isset($_POST['user']) && $_POST['user']!="") {
 ?>
 </p>
 
-
 <form method="post" action="oma_stiil.php">
-<table class="input">
+
+<table>
 		<tr>
        <td>Your text: <br /><br /></td>
-       <td><textarea name="user" cols="50" rows="10"> </textarea><br /><br /></td>
+       <td><textarea name="user" cols="50" rows="5" ><?php if (isset($_POST['user'])) echo $_POST['user'];?></textarea><br /><br /></td>
      </tr> 
  </table>
-<table class="textP">
+<table>
+       <tr>
        <td>Tekstivärvus<br /><br /></td>
-       <td> <input type="color" name="txt_col"><br /><br /></td>
+       <td> <input type="color" name="txt_col" value="<?php if (isset($_POST['txt_col'])) echo $_POST['txt_col']; ?>"><br /><br /></td>
     	 </tr> 
+    	 <tr>
     	  <td>Taustavärvus<br /><br /></td>
-       <td> <input type="color"name="bgr_col" value="#ff0000"><br /><br /></td>
+       <td> <input type="color" name="bgr_col" value= "<?php if (isset($_POST['bgr_col'])) echo $_POST['bgr_col']; ?>"><br /><br /></td>
     	 </tr> 
  </table>
  <table class="borderP">
      <tr>
-      <td>Border type:<br /><br /></td>
+      <td>Piirjoone tüüp:<br /><br /></td>
       <td>
        <select name="border">
+       <option value="<?php if (isset($_POST['border'])) {echo $_POST['border'];} else echo "select" ?>"><?php if (isset($_POST['border'])) {echo $_POST['border'];} else echo "select" ?></option>
         <option value="dotted">dotted</option>
         <option value="dashed">dashed</option>
         <option value="solid">solid</option>
@@ -75,19 +98,20 @@ if (isset($_POST['user']) && $_POST['user']!="") {
      </tr>
   		<tr>
        <td>Piirjoone laius (0-20px) <br /><br /></td>
-       <td> <input type="number" name="weight" min="0" max="20" step="1"><br /><br /></td>
+       <td> <input type="number" name="weight" value="<?php if (isset($_POST['weight'])) echo $_POST['weight']; ?>" min="0" max="20" step="1"><br /><br /></td>
     	 </tr> 
      <tr>
-     <tr>
        <td>Piirjoone nurga raadius (0-100px) <br /><br /></td>
-       <td> <input type="number" name="radius" min="0" max="100" step="1"><br /><br /></td>
+       <td> <input type="number" name="radius" value="<?php if (isset($_POST['radius'])) echo $_POST['radius']; ?>" min="0" max="100" step="1"><br /><br /></td>
     	 </tr> 
     <tr>
        <td>Piirjoone värvus<br /><br /></td>
-       <td> <input type="color" name="b_color"><br /><br /></td>
-    	 </tr>   
+       <td> <input type="color" name="b_color" value="<?php if (isset($_POST['b_color'])) echo $_POST['b_color']; ?>"><br /><br /></td>
+    	 </tr> 
+    	 <tr>  
      <td colspan=2>
      <button type="submit">Esita</button> </td>
+     </tr>
  </table> 
 </form>
 <br><br>
