23bd69a941386dcc2869ea9a56e4184c80c4aea2
Andrei Grigorjev <agrigorj@itcollege.ee>
2016-04-17 17:00:44 +0300

    ylesanne 9, p3

---
+++ 9.koduylesanne/galeriiLeht/Multipage/galerii.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Praktikum  - Ülesanne</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php require_once('head.html');?>
+
+<div id="wrap">
+	<h3>Fotod</h3>
+	<div id="gallery">
+<?php
+$files = array();
+$dir = opendir('pildid'); // open the cwd..also do an err check.
+while(false != ($file = readdir($dir))) {
+        if(($file != ".") and ($file != "..")) {
+                $files[] = $file; // put in array.
+        }   
+}
+natsort($files); // sort.
+// print.
+foreach($files as $file) {
+        echo("<img src='pildid/".$file."' alt='".$file."' />");
+}
+?>
+	</div>
+</div>
+<?php require_once('foot.html');?>
+</html>
---
+++ 9.koduylesanne/galeriiLeht/Multipage/tulemus.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Praktikum  - Ülesanne</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<?php require_once('head.html');?>
+<?php
+$files = array();
+$dir = opendir('pildid'); // open the cwd..also do an err check.
+while(false != ($file = readdir($dir))) {
+        if(($file != ".") and ($file != "..")) {
+                $files[] = $file; // put in array.
+        }   
+}
+?>
+<div id="wrap">
+	<h3>Valiku tulemus</h3>
+	<p>
+	<?php
+	if(!empty($_GET)) {
+		if (in_array($_GET['pilt'], $files)) {
+    		echo "Aitähh! Sinu hääl salvestatud!";
+				}else { echo "ERROR: Wrong value!";}
+
+	}else {
+		echo "Kas üldse ei meeldinud? Te ei valinud midagi!";
+	
+	}
+	?>
+	
+	</p>
+
+</div>
+<?php require_once('foot.html');?>
+</body>
+</html>
