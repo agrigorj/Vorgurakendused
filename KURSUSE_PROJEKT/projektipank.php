7ccf9f8ea681d57e05043f7e839b36165971ea57
Andrei Grigorjev <agrigorj@itcollege.ee>
2016-05-29 13:20:09 +0300

    kursuse projekt, alustatud

---
+++ KURSUSE_PROJEKT/projektipank.php
@@ -0,0 +1,33 @@
+<?php
+require_once('funk.php');
+session_start();
+connect_db();
+
+$page="pealeht";
+if (isset($_GET['page']) && $_GET['page']!=""){
+	$page=htmlspecialchars($_GET['page']);
+}
+
+include_once('views/head.html');
+
+switch($page){
+	case "login":
+		logi();
+	break;
+	case "projektid":
+		kuva_projektid();
+	break;
+	case "logout":
+		logout();
+	break;
+	case "lisa":
+		lisa();
+	break;
+	default:
+		include_once('views/pealeht.html');
+	break;
+}
+
+include_once('views/foot.html');
+
+?>
