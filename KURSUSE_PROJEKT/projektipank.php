62079e5e97eef7c0e7a0bcb6a05d7784bd1894f5
Andrei Grigorjev <agrigorj@itcollege.ee>
2016-06-01 23:30:25 +0300

    valmis hindamiseks

--- KURSUSE_PROJEKT/projektipank.php
+++ KURSUSE_PROJEKT/projektipank.php
@@ -1,24 +1,4 @@
 
-<script type="text/javascript">
-function altRows(id){
-	if(document.getElementsByTagName){  
-		
-		var table = document.getElementById(id);  
-		var rows = table.getElementsByTagName("tr"); 
-		 
-		for(i = 0; i < rows.length; i++){          
-			if(i % 2 == 0){
-				rows[i].className = "evenrowcolor";
-			}else{
-				rows[i].className = "oddrowcolor";
-			}      
-		}
-	}
-}
-window.onload=function(){
-	altRows('alternatecolor');
-}
-</script>
 
 <?php
 require_once('funk.php');
